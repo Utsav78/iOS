31afd35556f42e6fda30a78161b9c5aa7e87c17d
Utsav Budathoki <utsavbudathoki12@gmail.com>
2022-06-17 14:33:55 +0545

    added header viewmodel and date converter

---
+++ Project/Utils/Date+Utils.swift
@@ -0,0 +1,22 @@
+//
+//  Date+Utils.swift
+//  Project
+//
+//  Created by ebpearls on 6/17/22.
+//
+
+import Foundation
+
+extension Date {
+    static var bankeyDateFormatter: DateFormatter {
+        let formatter = DateFormatter()
+        formatter.timeZone = TimeZone(abbreviation: "MDT")
+        return formatter
+    }
+    
+    var monthDayYearString: String {
+        let dateFormatter = Date.bankeyDateFormatter
+        dateFormatter.dateFormat = "MMM d, yyyy"
+        return dateFormatter.string(from: self)
+    }
+}
