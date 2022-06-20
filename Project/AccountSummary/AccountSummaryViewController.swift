90f843173492f8fe623b8425c6220674afeaa739
Utsav Budathoki <utsavbudathoki12@gmail.com>
2022-06-20 15:13:00 +0545

    feat: add group netowork call to the account summary section using dispatchgroup

--- Project/AccountSummary/AccountSummaryViewController.swift
+++ Project/AccountSummary/AccountSummaryViewController.swift
@@ -27,20 +27,19 @@ class AccountSummaryViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setup()
-        setupNavigationBar()
     }
     
-    func setupNavigationBar() {
-        navigationItem.rightBarButtonItem = logoutBarButtonItem
-    }
+ 
 }
 
 extension AccountSummaryViewController {
     private func setup() {
+        setupNavigationBar()
+
         setupTableView()
         setupTableHeaderView()
 //        fetchAccounts()
-        fetchDataAndLoadViews()
+        fetchData()
     }
     
     private func setupTableView() {
@@ -70,6 +69,9 @@ extension AccountSummaryViewController {
          
         tableView.tableHeaderView = headerView
         
+    }
+    func setupNavigationBar() {
+        navigationItem.rightBarButtonItem = logoutBarButtonItem
     }
 }
 
@@ -137,28 +139,43 @@ extension AccountSummaryViewController {
 }
 // MARK: - Networking
 extension AccountSummaryViewController {
-    private func fetchDataAndLoadViews() {
+    private func fetchData() {
         
+        let group = DispatchGroup()
+        
+        
+        group.enter()
         fetchProfile(forUserId: "1") { result in
             switch result {
             case .success(let profile):
                 self.profile = profile
                 self.configureTableHeaderView(with: profile)
-                self.tableView.reloadData()
+//                self.tableView.reloadData()
             case .failure(let error):
                 print(error.localizedDescription)
             }
+            group.leave()
         }
+        
+        group.enter()
         fetchAccounts(forUserId: "1") { result in
             switch result {
             case .success(let accounts):
                 self.accounts = accounts
                 self.configureTableCells(with: accounts)
-                self.tableView.reloadData()
+//                self.tableView.reloadData()
             case .failure(let error):
                 print(error.localizedDescription)
             }
+            group.leave()
         }
+        
+        group.notify(queue: .main) {
+            self.tableView.reloadData()
+        }
+        
+        
+        
     }
     
     private func configureTableHeaderView(with profile: Profile) {
