b2f311b2097c7f9cc29b0a6ba979bb4e7bc2a56e
Utsav Budathoki <utsavbudathoki12@gmail.com>
2022-06-22 10:58:26 +0545

    added layout for new password field

---
+++ PasswordReset/PasswordReset/PasswordTextField.swift
@@ -0,0 +1,38 @@
+//
+//  PasswordTextField.swift
+//  PasswordReset
+//
+//  Created by ebpearls on 6/22/22.
+//
+
+import UIKit
+
+class PasswordTextField: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        style()
+        layout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override var intrinsicContentSize: CGSize {
+        return CGSize(width: 200, height: 200)
+    }
+}
+
+extension PasswordTextField {
+    
+    func style() {
+        translatesAutoresizingMaskIntoConstraints = false
+        backgroundColor = .systemOrange
+    }
+    
+    func layout() {
+        
+    }
+}
