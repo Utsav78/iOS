e708416ef80e988f2952f80c9201b50f5f1e68fc
Utsav Budathoki <utsavbudathoki12@gmail.com>
2024-01-20 10:14:28 +1030

    signupview configuration

---
+++ Project/Signup/SignupView.swift
@@ -0,0 +1,19 @@
+//
+//  SignupView.swift
+//  Project
+//
+//  Created by Utsav Budathoki on 20/1/2024.
+//
+
+import Foundation
+import UIKit
+
+class SignupView: UIView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
---
+++ Project/Signup/SignupViewController.swift
@@ -0,0 +1,8 @@
+//
+//  SignupViewController.swift
+//  Project
+//
+//  Created by Utsav Budathoki on 20/1/2024.
+//
+
+import Foundation
