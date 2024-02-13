b08473bea57588f49181cd8bbcc7e4a6a102e99d
Utsav Budathoki <utsavbudathoki12@gmail.com>
2024-02-13 15:11:46 +1030

    feat: Add MapView

--- Project/Components/CircleImage.swift
+++ Project/Components/CircleImage.swift
@@ -10,13 +10,10 @@ import SwiftUI
 struct CircleImage: View {
     var image: Image
     
+    
     var body: some View {
         image
             .clipShape(Circle())
-            .overlay {
-                Circle().stroke(.white, lineWidth: 4)
-                    .shadow(radius: 7)
-            }
     }
 }
 
